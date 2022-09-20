edfb7e9db10ef1a7627c9c9bb862d8daba5116c0
mobasaradev <mkmobasara.dev@gmail.com>
2022-09-21 00:26:26 +0600

    add validity in login form

--- lib/main.dart
+++ lib/main.dart
@@ -15,27 +15,70 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Catalog"),
+      home: FormPage(),
+    );
+  }
+}
+
+class FormPage extends StatelessWidget {
+  FormPage({super.key});
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "example@gmail.com",
+                ),
+                validator: (email) {
+                  if (email!.isEmpty) {
+                    return "Enter your email";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "123!@#Pass",
+                ),
+                validator: (pass) {
+                  if (pass!.isEmpty) {
+                    return "Enter your Password";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Processing Data'),
+                        backgroundColor: Colors.blueAccent,
+                      ),
+                    );
+                  }
+                },
+                child: const Text("Log In"),
+              ),
+            ],
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "email",
-                  ),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Password",
-                  ),
-                ),
-              ],
-            ),
-          )),
+        ),
+      ),
     );
   }
 }
