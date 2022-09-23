8db704b6847be554f2719aa7408e3da081e23fb3
mobasaradev <mkmobasara.dev@gmail.com>
2022-09-23 20:58:17 +0600

    create facebook ui

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -6,7 +6,7 @@ class HomePages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
@@ -14,7 +14,7 @@ class HomePages extends StatelessWidget {
               const CircleAvatar(
                 backgroundImage: NetworkImage(
                     'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80'),
-                radius: 50,
+                radius: 100,
               ),
               const SizedBox(
                 height: 10,
@@ -60,7 +60,77 @@ class HomePages extends StatelessWidget {
                     ),
                   ),
                 ],
-              )
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const ListTile(
+                leading: Icon(Icons.school),
+                title: Text("Graduated in English"),
+              ),
+              const ListTile(
+                leading: Icon(Icons.home_work_outlined),
+                title: Text("Collage in Humanity"),
+              ),
+              const ListTile(
+                leading: Icon(Icons.home_work_outlined),
+                title: Text("High school in Science"),
+              ),
+              const Divider(
+                thickness: 1,
+                color: Colors.grey,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Most Mobasara Khatun's post",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Text(
+                    "Most recent post",
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                  Image.network(
+                    'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
+                    height: 400,
+                    fit: BoxFit.cover,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: const [
+                      Icon(Icons.thumb_up),
+                      Text("Like"),
+                    ],
+                  ),
+                  Row(
+                    children: const [
+                      Icon(Icons.comment),
+                      Text("Comments"),
+                    ],
+                  ),
+                  Row(
+                    children: const [
+                      Icon(Icons.share),
+                      Text("Share"),
+                    ],
+                  ),
+                ],
+              ),
             ],
           ),
         ),
